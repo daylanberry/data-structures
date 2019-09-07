4acf0cd0c23d57f2bc2ae272a66d1ad425663828
daylanberry <daylanberry@gmail.com>
2019-09-06 20:21:36 -0700

    linked

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // list.tail = value
+    var newTail = new Node(value);
+    if (list.head === null && list.tail === null) {
+      list.head = newTail;
+      list.tail = newTail;
+    } else {
+      var previousTail = list.tail;
+      previousTail.next = newTail;
+      list.tail = newTail;
+    }
+    //if head = null, point to new node
+    //list.tail = create new node
+    //if list.tail has an old node, old node points to new key and list.tail points to new key
   };
 
   list.removeHead = function() {
+    //if head is null, return undefined or null
+    //head points to node.next.next
+    //delete node
   };
 
   list.contains = function(target) {
+    for (var prop in list.head){
+      if (type )
+    }
   };
 
   return list;
